efd8de434dd8ffdeecebba0001fd41dcecabbecb
sh596 <sh596@naver.com>
2022-01-03 19:15:16 +0900

    ui

---
+++ app/src/main/java/com/example/bakinapplication/ui/account/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bakinapplication.ui.account
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bakinapplication.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
