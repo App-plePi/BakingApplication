4c22660f226f1c5dabc4e856b5329efb8b14a02f
sh596 <sh596@naver.com>
2022-01-05 00:15:16 +0900

    ui complete

--- app/src/main/java/com/example/bakinapplication/ui/main/RecipeCreateActivity.kt
+++ app/src/main/java/com/example/bakinapplication/ui/main/RecipeCreateActivity.kt
@@ -1,11 +1,11 @@
-package com.example.bakinapplication
+package com.example.bakinapplication.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RecipeCreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recipe_create)
     }
 }
